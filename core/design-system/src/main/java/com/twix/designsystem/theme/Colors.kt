f5f7759caee14fca429e0ccda3261a2fd03d0cbf
chanho0908 <uni070@naver.com>
2026-02-19 00:06:06 +0900

    ✨ Feat: 스탬프 컬러 리소스 추가

--- core/design-system/src/main/java/com/twix/designsystem/theme/Colors.kt
+++ core/design-system/src/main/java/com/twix/designsystem/theme/Colors.kt
@@ -24,3 +24,14 @@ object SystemColor {
     val Success = Color(0xFF1ED45A)
     val Warning = Color(0xFFFF6363)
 }
+
+object ChromaticColor {
+    val Green400 = Color(0xFFBEF0A3)
+    val Blue400 = Color(0xFF91E2F4)
+    val Yellow400 = Color(0xFFFFEB78)
+    val Pink400 = Color(0xFFFF96AD)
+    val Pink300 = Color(0xFFFFC0CE)
+    val Pink200 = Color(0xFFFFD5DE)
+    val Orange400 = Color(0xFFFFA271)
+    val Purple400 = Color(0xFFDCC3FF)
+}

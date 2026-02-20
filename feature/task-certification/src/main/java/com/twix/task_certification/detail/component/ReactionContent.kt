5595ff2cce54cf5447696abb964e3fa0636ae8a3
chanho0908 <uni070@naver.com>
2026-02-20 13:14:17 +0900

    ♻️ Refactor: ReactionContent 레이아웃 구조 개선
    - Column 및 Spacer 제거
    - Box에 직접 top padding을 적용하여 레이아웃 단순화

--- feature/task-certification/src/main/java/com/twix/task_certification/detail/component/ReactionContent.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/detail/component/ReactionContent.kt
@@ -1,11 +1,7 @@
 package com.twix.task_certification.detail.component
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,22 +24,22 @@ internal fun ReactionContent(
 ) {
     var effectTarget by remember { mutableStateOf<ReactionUiModel?>(null) }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-        ) {
-            Spacer(Modifier.height(85.dp))
-            ReactionBar(
-                selectedReaction = reaction,
-                onSelectReaction = { type ->
-                    onClickReaction(type)
-                    effectTarget = ReactionUiModel.find(type)
-                },
-                modifier =
-                    Modifier
-                        .padding(horizontal = 20.dp),
-            )
-        }
+    Box(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .padding(top = 85.dp),
+    ) {
+        ReactionBar(
+            selectedReaction = reaction,
+            onSelectReaction = { type ->
+                onClickReaction(type)
+                effectTarget = ReactionUiModel.find(type)
+            },
+            modifier =
+                Modifier
+                    .padding(horizontal = 20.dp),
+        )
 
         ReactionEffect(
             targetReaction = effectTarget,

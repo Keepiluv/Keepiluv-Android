d949340b66b3c3ffe83d0d7a10124f27b7977424
chanho0908 <uni070@naver.com>
2026-02-20 13:16:54 +0900

    ✨ Feat: 인증 상세 화면 TopBar Preview 추가

--- feature/task-certification/src/main/java/com/twix/task_certification/detail/component/TaskCertificationDetailTopBar.kt
+++ feature/task-certification/src/main/java/com/twix/task_certification/detail/component/TaskCertificationDetailTopBar.kt
@@ -3,6 +3,7 @@ package com.twix.task_certification.detail.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,11 +12,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.twix.designsystem.components.text.AppText
 import com.twix.designsystem.components.topbar.CommonTopBar
 import com.twix.designsystem.theme.CommonColor
 import com.twix.designsystem.theme.GrayColor
+import com.twix.designsystem.theme.TwixTheme
 import com.twix.domain.model.enums.AppTextStyle
 import com.twix.ui.extension.noRippleClickable
 import com.twix.designsystem.R as DesR
@@ -60,4 +63,26 @@ internal fun TaskCertificationDetailTopBar(
         },
         modifier = Modifier.background(color = CommonColor.White),
     )
+}
+
+@Preview
+@Composable
+fun TaskCertificationDetailTopBarPreview() {
+    TwixTheme {
+        Column {
+            TaskCertificationDetailTopBar(
+                title = "목표 인증",
+                canModify = true,
+                onBack = {},
+                onClickModify = {},
+            )
+
+            TaskCertificationDetailTopBar(
+                title = "목표 인증",
+                canModify = false,
+                onBack = {},
+                onClickModify = {},
+            )
+        }
+    }
 }

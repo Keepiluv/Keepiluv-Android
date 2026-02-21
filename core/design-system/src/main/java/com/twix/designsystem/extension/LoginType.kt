7387e7050b7c648b8aa2ed6d21262d59f895e268
chanho0908 <uni070@naver.com>
2026-02-21 21:55:03 +0900

    ✨ Feat: 카카오 로그인 버튼 UI 모델 추가
    - Google 로그인 버튼 색상 변경
    - Kakao 로그인 버튼에 맞는 아이콘, 배경색, 테두리색 지정

--- core/design-system/src/main/java/com/twix/designsystem/extension/LoginType.kt
+++ core/design-system/src/main/java/com/twix/designsystem/extension/LoginType.kt
@@ -2,8 +2,8 @@ package com.twix.designsystem.extension
 
 import com.twix.designsystem.R
 import com.twix.designsystem.components.button.model.LoginTypeUiModel
-import com.twix.designsystem.theme.CommonColor
 import com.twix.designsystem.theme.GrayColor
+import com.twix.designsystem.theme.LoginColor
 import com.twix.domain.model.enums.LoginType
 
 fun LoginType.toUiModel(): LoginTypeUiModel =
@@ -13,18 +13,17 @@ fun LoginType.toUiModel(): LoginTypeUiModel =
                 type = this,
                 logo = R.drawable.ic_google,
                 title = R.string.google_login_button_title,
-                background = CommonColor.White,
+                background = LoginColor.Google,
                 border = GrayColor.C200,
                 textColor = GrayColor.C500,
             )
-        // TODO : KAKAO용으로 수정
         LoginType.KAKAO ->
             LoginTypeUiModel(
                 type = this,
-                logo = R.drawable.ic_google,
+                logo = R.drawable.ic_kakao,
                 title = R.string.google_login_button_title,
-                background = CommonColor.White,
-                border = GrayColor.C200,
+                background = LoginColor.Kakao,
+                border = LoginColor.Kakao,
                 textColor = GrayColor.C500,
             )
     }

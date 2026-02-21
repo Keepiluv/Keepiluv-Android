481b890920fdc0a71ab318a285f920aee8451611
chanho0908 <uni070@naver.com>
2026-02-21 18:35:33 +0900

    ✨ Feat: 카카오 로그인 API 추가

--- data/src/main/java/com/twix/data/repository/DefaultAuthRepository.kt
+++ data/src/main/java/com/twix/data/repository/DefaultAuthRepository.kt
@@ -16,10 +16,11 @@ class DefaultAuthRepository(
         idToken: String,
         type: LoginType,
     ) {
+        val request = LoginRequest(idToken)
         val response =
             when (type) {
-                LoginType.GOOGLE -> service.googleLogin(LoginRequest(idToken))
-                LoginType.KAKAO -> return
+                LoginType.GOOGLE -> service.googleLogin(request)
+                LoginType.KAKAO -> service.kakaoLogin(request)
             }
 
         tokenProvider.saveToken(response.accessToken, response.refreshToken)

9e9cb0f371cf70cce6687a0ad363bad1772bbc14
dogmania <hj296914@gmail.com>
2026-02-24 03:31:42 +0900

    ✨ Faet: 필요 의존성 추가

--- core/notification/build.gradle.kts
+++ core/notification/build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     alias(libs.plugins.twix.android.library)
     alias(libs.plugins.twix.koin)
+    alias(libs.plugins.twix.kermit)
+    alias(libs.plugins.google.services)
 }
 
 android {
     namespace = "com.twix.notification"
+}
+
+dependencies {
+    implementation(projects.domain)
+    implementation(projects.core.result)
+    implementation(projects.core.deviceContract)
+    implementation(projects.core.navigationContract)
+
+    implementation(platform(libs.google.firebase.bom))
+    implementation(libs.google.firebase.messaging)
 }

461ce0197bcc4b94d818d8f0a3e37a02a26f1c64
dogmania <hj296914@gmail.com>
2026-02-24 03:26:49 +0900

    ✨ Feat: Fcm 토큰 등록 로직 구현

--- app/src/main/java/com/yapp/twix/TwixApplication.kt
+++ app/src/main/java/com/yapp/twix/TwixApplication.kt
@@ -1,14 +1,25 @@
 package com.yapp.twix
 
 import android.app.Application
+import co.touchlab.kermit.Logger
+import com.twix.notification.token.NotificationTokenRegistrar
 import com.yapp.twix.di.initKoin
+import org.koin.java.KoinJavaComponent.getKoin
 
 class TwixApplication : Application() {
+    private val logger = Logger.withTag("TwixApplication")
+
     override fun onCreate() {
         super.onCreate()
 
         initKoin(
             context = this,
         )
+
+        try {
+            getKoin().get<NotificationTokenRegistrar>().registerCurrentToken()
+        } catch (e: Exception) {
+            logger.e(e) { "FCM token 등록 실패" }
+        }
     }
 }

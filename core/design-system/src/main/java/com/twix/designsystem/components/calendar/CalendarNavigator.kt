5e913171283df3b9199468e353c097c23d4f0aa3
chanho0908 <uni070@naver.com>
2026-02-23 22:46:14 +0900

    ♻️ Refactor: 캘린더 네비게이터 클릭 영역 수정
    - `padding`과 `noRippleClickable`의 순서를 변경하여 클릭 영역을 아이콘 외부 패딩까지 확장

--- core/design-system/src/main/java/com/twix/designsystem/components/calendar/CalendarNavigator.kt
+++ core/design-system/src/main/java/com/twix/designsystem/components/calendar/CalendarNavigator.kt
@@ -39,9 +39,9 @@ fun CalendarNavigator(
             contentDescription = "previous month",
             modifier =
                 Modifier
-                    .padding(6.dp)
                     .size(24.dp)
-                    .noRippleClickable(onClick = onPreviousMonth),
+                    .noRippleClickable(onClick = onPreviousMonth)
+                    .padding(6.dp),
         )
 
         AppText(
@@ -56,9 +56,9 @@ fun CalendarNavigator(
             contentDescription = "next month",
             modifier =
                 Modifier
-                    .padding(6.dp)
                     .size(24.dp)
-                    .noRippleClickable(onClick = onNextMonth),
+                    .noRippleClickable(onClick = onNextMonth)
+                    .padding(6.dp),
         )
     }
 }

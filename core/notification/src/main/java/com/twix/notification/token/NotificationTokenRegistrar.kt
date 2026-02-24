f5f554bcb3c2edc089e7de66982c28009c2ea7c6
dogmania <hj296914@gmail.com>
2026-02-24 22:41:11 +0900

    ✨ Feat: fcm 토큰 등록 로직 구현

--- core/notification/src/main/java/com/twix/notification/token/NotificationTokenRegistrar.kt
+++ core/notification/src/main/java/com/twix/notification/token/NotificationTokenRegistrar.kt
@@ -3,8 +3,7 @@ package com.twix.notification.token
 import co.touchlab.kermit.Logger
 import com.google.firebase.messaging.FirebaseMessaging
 import com.twix.device_contract.IdProvider
-import com.twix.domain.model.user.User
-import com.twix.domain.repository.UserRepository
+import com.twix.domain.repository.NotificationRepository
 import com.twix.result.AppResult
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -12,8 +11,7 @@ import kotlinx.coroutines.tasks.await
 import kotlin.coroutines.cancellation.CancellationException
 
 class NotificationTokenRegistrar(
-//    private val notificationRepository: NotificationRepository,
-    private val userRepository: UserRepository,
+    private val notificationRepository: NotificationRepository,
     private val deviceIdProvider: IdProvider,
     private val appScope: CoroutineScope,
 ) {
@@ -51,22 +49,24 @@ class NotificationTokenRegistrar(
     }
 
     private suspend fun registerInternal(fcmToken: String) {
-        val userResult = userRepository.fetchUserInfo()
+        if (fcmToken.isBlank()) {
+            logger.d { "FCM token이 비어있음" }
+            return
+        }
+
         val deviceId = deviceIdProvider.getOrCreateDeviceId()
+        val result =
+            notificationRepository.registerFcmToken(
+                deviceId = deviceId,
+                token = fcmToken,
+            )
 
-        when (userResult) {
+        when (result) {
             is AppResult.Error -> {
-                logger.w { "FCM token 등록 스킵 - 사용자 정보 조회 실패: ${userResult.error}" }
-                return
+                logger.e { "FCM token 등록 실패: ${result.error}" }
             }
-            is AppResult.Success<User> -> {
-//                notificationRepository.registerFcmToken(
-//                    userId = user.data.id,
-//                    deviceId = deviceId,
-//                    fcmToken = fcmToken,
-//                )
-
-                logger.i("FCM token registered. userId=${userResult.data.id}, deviceId=$deviceId")
+            is AppResult.Success -> {
+                logger.d { "FCM token 등록 성공" }
             }
         }
     }

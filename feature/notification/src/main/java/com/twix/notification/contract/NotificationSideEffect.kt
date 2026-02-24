4686fd8d08625a8a3ceb14cd0c94c8c94ef3e3b7
dogmania <hj296914@gmail.com>
2026-02-25 02:30:38 +0900

    ✨ Feat: NotificationSideEffect 정의

--- feature/notification/src/main/java/com/twix/notification/contract/NotificationSideEffect.kt
+++ feature/notification/src/main/java/com/twix/notification/contract/NotificationSideEffect.kt
@@ -1,7 +1,17 @@
 package com.twix.notification.contract
 
 import com.twix.ui.base.SideEffect
+import java.time.LocalDate
 
-interface NotificationSideEffect: SideEffect {
-
+interface NotificationSideEffect : SideEffect {
+    data object NavigateToHome : NotificationSideEffect
+    data class NavigateToPartnerPhotolog(
+        val goalId: Long,
+        val date: LocalDate,
+    ) : NotificationSideEffect
+    data class NavigateToMyPhotolog(
+        val goalId: Long,
+        val date: LocalDate,
+    ) : NotificationSideEffect
+    data object NavigateToStatisticsEndedGoals : NotificationSideEffect
 }

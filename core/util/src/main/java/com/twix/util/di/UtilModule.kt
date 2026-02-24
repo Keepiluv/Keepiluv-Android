f2d48198809197a4ce6bc644678ed17fd6b9c6f0
chanho0908 <uni070@naver.com>
2026-02-24 14:31:42 +0900

    Revert "✨ Feat: StatsRefreshBus 추가"
    This reverts commit 904b2e976c0a0a4d2c2f9ca8e7c8386996937ac0.

--- core/util/src/main/java/com/twix/util/di/UtilModule.kt
+++ core/util/src/main/java/com/twix/util/di/UtilModule.kt
@@ -1,7 +1,6 @@
 package com.twix.util.di
 
 import com.twix.util.bus.GoalRefreshBus
-import com.twix.util.bus.StatsRefreshBus
 import com.twix.util.bus.TaskCertificationRefreshBus
 import org.koin.dsl.module
 
@@ -9,5 +8,4 @@ val utilModule =
     module {
         single { GoalRefreshBus() }
         single { TaskCertificationRefreshBus() }
-        single { StatsRefreshBus() }
     }

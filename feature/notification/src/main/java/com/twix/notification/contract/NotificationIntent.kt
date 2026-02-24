769cee7679f043e4cc680765959189d8d2d75ec6
dogmania <hj296914@gmail.com>
2026-02-25 02:30:26 +0900

    ✨ Feat: NotificationIntent 구현

--- feature/notification/src/main/java/com/twix/notification/contract/NotificationIntent.kt
+++ feature/notification/src/main/java/com/twix/notification/contract/NotificationIntent.kt
@@ -2,9 +2,10 @@ package com.twix.notification.contract
 
 import com.twix.ui.base.Intent
 
-sealed interface NotificationIntent: Intent {
-    data object NextPage: NotificationIntent
+sealed interface NotificationIntent : Intent {
+    data object FetchNextPage : NotificationIntent
+
     data class NotificationClicked(
         val notificationId: Long,
-    ): NotificationIntent
+    ) : NotificationIntent
 }

89077f100e31d8c773aae2197d7e1bb256038af8
dogmania <hj296914@gmail.com>
2026-02-25 02:29:33 +0900

    ✨ Feat: 알림 리스트 컴포넌트 구현

---
+++ feature/notification/src/main/java/com/twix/notification/component/NotificationList.kt
@@ -0,0 +1,70 @@
+package com.twix.notification.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twix.designsystem.theme.GrayColor
+import com.twix.designsystem.theme.TwixTheme
+import com.twix.domain.model.enums.NotificationType
+import com.twix.domain.model.goal.Notification
+
+@Composable
+fun NotificationList(
+    modifier: Modifier = Modifier,
+    notificationsList: List<Notification>,
+    onNotificationClick: (Long) -> Unit,
+) {
+    LazyColumn(
+        modifier = modifier.padding(horizontal = 20.dp),
+    ) {
+        itemsIndexed(
+            items = notificationsList,
+            key = { _, item -> item.id },
+        ) { index, notification ->
+            NotificationItem(
+                notification = notification,
+                onClick = onNotificationClick,
+            )
+
+            if (index != notificationsList.lastIndex) {
+                HorizontalDivider(thickness = 1.dp, color = GrayColor.C100)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun Preview() {
+    TwixTheme {
+        NotificationList(
+            notificationsList =
+                listOf(
+                    Notification(
+                        id = 1,
+                        title = "titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle",
+                        body = "content",
+                        type = NotificationType.GOAL_COMPLETED,
+                        isRead = false,
+                        deepLink = null,
+                        createdAt = null,
+                    ),
+                    Notification(
+                        id = 2,
+                        title = "titletitletitletitletitletitletitletitletitletitle",
+                        body = "content",
+                        type = NotificationType.GOAL_COMPLETED,
+                        isRead = false,
+                        deepLink = null,
+                        createdAt = null,
+                    ),
+                ),
+            onNotificationClick = {},
+        )
+    }
+}

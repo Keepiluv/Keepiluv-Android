eed47651d73ccd91edd3f3016dbf239cab50b6fb
chanho0908 <uni070@naver.com>
2026-02-24 18:01:00 +0900

    ✨ Design: StatsScreen 탭 레이아웃 및 UI 세부 수정
    - `PrimaryTabRow`에 좌우 패드(20.dp)를 적용하고, 인디케이터에 설정되어 있던 패딩 제거
    - 탭 텍스트 하단에 패딩(12.dp)을 추가하여 간격 조정
    - `StatsRoutePreview`에 `showBackground = true` 속성 추가

--- feature/main/src/main/java/com/twix/stats/StatsScreen.kt
+++ feature/main/src/main/java/com/twix/stats/StatsScreen.kt
@@ -106,6 +106,7 @@ private fun StatsTabRow(pagerState: PagerState) {
     val coroutineScope = rememberCoroutineScope()
 
     PrimaryTabRow(
+        modifier = Modifier.padding(horizontal = 20.dp),
         selectedTabIndex = pagerState.currentPage,
         indicator = {
             TabRowDefaults.PrimaryIndicator(
@@ -114,7 +115,6 @@ private fun StatsTabRow(pagerState: PagerState) {
                         .tabIndicatorOffset(pagerState.currentPage)
                         .fillMaxWidth()
                         .height(1.4.dp)
-                        .padding(horizontal = 20.dp)
                         .background(GrayColor.C500),
                 color = GrayColor.C500,
             )
@@ -129,6 +129,7 @@ private fun StatsTabRow(pagerState: PagerState) {
                         text = stringResource(destination.label),
                         style = AppTextStyle.T2,
                         color = if (isSelected) GrayColor.C500 else GrayColor.C200,
+                        modifier = Modifier.padding(bottom = 12.dp)
                     )
                 },
                 selected = isSelected,
@@ -179,7 +180,7 @@ private val noRippleInteractionSource =
         override fun tryEmit(interaction: Interaction) = true
     }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun StatsRoutePreview(
     @PreviewParameter(StatsUiStatePreviewProvider::class)

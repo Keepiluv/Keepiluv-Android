49bfc7cbd4b4e01bfb85fb638e871527e656caf9
dogmania <hj296914@gmail.com>
2026-02-26 03:23:16 +0900

    ✨ Feat: 찌르기 버튼 콜백 추가

--- feature/main/src/main/java/com/twix/home/HomeScreen.kt
+++ feature/main/src/main/java/com/twix/home/HomeScreen.kt
@@ -145,6 +145,7 @@ fun HomeRoute(
         onClickCard = navigateToCertificationDetail,
         onSettingClick = navigateToSettings,
         onNotificationClick = navigateToNotification,
+        onPokeGoal = { viewModel.dispatch(HomeIntent.PokeGoal(it)) }
     )
 }
 
@@ -163,6 +164,7 @@ fun HomeScreen(
     onClickCard: (Long, LocalDate, BetweenUs) -> Unit,
     onSettingClick: () -> Unit,
     onNotificationClick: () -> Unit,
+    onPokeGoal: (Long) -> Unit,
 ) {
     Box(
         modifier =
@@ -209,6 +211,7 @@ fun HomeScreen(
                     onVerificationClick = onVerificationClick,
                     onEditClick = onEditClick,
                     onClickGoalCard = onClickCard,
+                    onPokeGoal = onPokeGoal,
                 )
             }
         }
@@ -231,6 +234,7 @@ fun GoalList(
     onVerificationClick: (Long, GoalCheckState) -> Unit,
     onClickGoalCard: (Long, LocalDate, BetweenUs) -> Unit,
     onEditClick: () -> Unit,
+    onPokeGoal: (Long) -> Unit,
 ) {
     val today = remember { LocalDate.now() }
     val titleRes =
@@ -306,6 +310,7 @@ fun GoalList(
                                     BetweenUs.PARTNER,
                                 )
                             },
+                            onPokeGoal = { onPokeGoal(goal.goalId) },
                         )
                     }
                 },

aaf0d433167874a30a0544cfec7abbd8bb36fd29
dogmania <hj296914@gmail.com>
2026-02-25 23:38:10 +0900

    ♻️ Refactor: hasNext 기본값 변경

--- feature/notification/src/main/java/com/twix/notification/contract/NotificationUiState.kt
+++ feature/notification/src/main/java/com/twix/notification/contract/NotificationUiState.kt
@@ -1,9 +1,11 @@
 package com.twix.notification.contract
 
+import androidx.compose.runtime.Immutable
 import com.twix.domain.model.notification.Notification
 import com.twix.ui.base.State
 
+@Immutable
 data class NotificationUiState(
     val notificationList: List<Notification> = emptyList(),
-    val hasNext: Boolean = false,
+    val hasNext: Boolean = true,
 ) : State

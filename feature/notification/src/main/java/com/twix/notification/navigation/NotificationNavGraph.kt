e368e6c75c25a32c971976913109f35184fa0e85
dogmania <hj296914@gmail.com>
2026-02-25 23:38:40 +0900

    ✨ Feat: NotificationScreen 네비게이션 로직 구현

--- feature/notification/src/main/java/com/twix/notification/navigation/NotificationNavGraph.kt
+++ feature/notification/src/main/java/com/twix/notification/navigation/NotificationNavGraph.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.twix.domain.model.enums.BetweenUs
 import com.twix.navigation.NavRoutes
 import com.twix.navigation.base.NavGraphContributor
 import com.twix.notification.NotificationRoute
@@ -22,6 +23,26 @@ object NotificationNavGraph : NavGraphContributor {
             composable(NavRoutes.NotificationRoute.route) {
                 NotificationRoute(
                     popBackStack = { navController.popBackStack() },
+                    navigateToMyPhotolog = { goalId, date ->
+                        navController.navigate(NavRoutes.TaskCertificationDetailRoute.createRoute(goalId, date, BetweenUs.ME.name)) {
+                            launchSingleTop = true
+                        }
+                    },
+                    navigateToPartnerPhotolog = { goalId, date ->
+                        navController.navigate(
+                            NavRoutes.TaskCertificationDetailRoute.createRoute(
+                                goalId,
+                                date,
+                                BetweenUs.PARTNER.name,
+                            ),
+                        ) {
+                            launchSingleTop = true
+                        }
+                    },
+                    navigateToStatisticsEndedGoals = {
+                        // TODO: 통계 화면으로 이동하는 로직
+                        navController.popBackStack()
+                    },
                 )
             }
         }

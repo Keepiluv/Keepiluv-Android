4f34e02255299a233be43ab70855997149243e33
dogmania <hj296914@gmail.com>
2026-02-26 03:22:16 +0900

    ✨ Feat: PokeRepository Koin 컨테이너 등록

--- data/src/main/java/com/twix/data/di/RepositoryModule.kt
+++ data/src/main/java/com/twix/data/di/RepositoryModule.kt
@@ -5,6 +5,7 @@ import com.twix.data.repository.DefaultGoalRepository
 import com.twix.data.repository.DefaultNotificationRepository
 import com.twix.data.repository.DefaultOnboardingRepository
 import com.twix.data.repository.DefaultPhotoLogRepository
+import com.twix.data.repository.DefaultPokeRepository
 import com.twix.data.repository.DefaultStatsRepository
 import com.twix.data.repository.DefaultUserRepository
 import com.twix.domain.repository.AuthRepository
@@ -12,6 +13,7 @@ import com.twix.domain.repository.GoalRepository
 import com.twix.domain.repository.NotificationRepository
 import com.twix.domain.repository.OnBoardingRepository
 import com.twix.domain.repository.PhotoLogRepository
+import com.twix.domain.repository.PokeRepository
 import com.twix.domain.repository.StatsRepository
 import com.twix.domain.repository.UserRepository
 import org.koin.dsl.module
@@ -38,5 +40,8 @@ internal val repositoryModule =
         }
         single<NotificationRepository> {
             DefaultNotificationRepository(get())
+        }
+        single<PokeRepository> {
+            DefaultPokeRepository(get())
         }
     }

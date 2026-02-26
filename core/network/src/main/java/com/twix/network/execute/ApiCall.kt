7a8c2e059f50424dd313ed36b6183bed70fe80b9
dogmania <hj296914@gmail.com>
2026-02-26 23:52:21 +0900

    ♻️ Refactor: IOException 임포트 수정

--- core/network/src/main/java/com/twix/network/execute/ApiCall.kt
+++ core/network/src/main/java/com/twix/network/execute/ApiCall.kt
@@ -7,7 +7,7 @@ import io.ktor.client.plugins.ResponseException
 import io.ktor.client.statement.bodyAsText
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
-import java.io.IOException
+import kotlinx.io.IOException
 import java.net.SocketTimeoutException
 import kotlin.coroutines.cancellation.CancellationException
 

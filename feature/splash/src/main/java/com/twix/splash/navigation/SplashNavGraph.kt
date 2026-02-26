fb69524c56c1c9e9c68a0a198c6ddad918415398
dogmania <hj296914@gmail.com>
2026-02-26 23:14:47 +0900

    ✨ Feat: SplashNavGraph 구현

---
+++ feature/splash/src/main/java/com/twix/splash/navigation/SplashNavGraph.kt
@@ -0,0 +1,32 @@
+package com.twix.splash.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.twix.navigation.NavRoutes
+import com.twix.navigation.base.NavGraphContributor
+import com.twix.splash.SplashRoute
+
+object SplashNavGraph : NavGraphContributor {
+    override val graphRoute: NavRoutes
+        get() = NavRoutes.SplashGraph
+    override val startDestination: String
+        get() = NavRoutes.SplashRoute.route
+    override val priority: Int
+        get() = 0
+
+    override fun NavGraphBuilder.registerGraph(navController: NavHostController) {
+        navigation(
+            route = graphRoute.route,
+            startDestination = startDestination,
+        ) {
+            composable(NavRoutes.SplashRoute.route) {
+                SplashRoute(
+                    navigateToMain = { navController.navigate(NavRoutes.MainGraph.route) },
+                    navigateToLogin = { navController.navigate(NavRoutes.LoginGraph.route) },
+                )
+            }
+        }
+    }
+}

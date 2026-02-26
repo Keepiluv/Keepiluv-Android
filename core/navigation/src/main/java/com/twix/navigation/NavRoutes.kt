e0ccf6d5e19c1d6811e946449c8a1006ac32072f
dogmania <hj296914@gmail.com>
2026-02-26 21:57:12 +0900

    ✨ Feat: SplashGraph NavRoutes 정의

--- core/navigation/src/main/java/com/twix/navigation/NavRoutes.kt
+++ core/navigation/src/main/java/com/twix/navigation/NavRoutes.kt
@@ -148,4 +148,11 @@ sealed class NavRoutes(
     object NotificationGraph : NavRoutes("notification_graph")
 
     object NotificationRoute : NavRoutes("notification")
+
+    /**
+     * SplashGraph
+     * */
+    object SplashGraph : NavRoutes("splash_graph")
+
+    object SplashRoute : NavRoutes("splash")
 }

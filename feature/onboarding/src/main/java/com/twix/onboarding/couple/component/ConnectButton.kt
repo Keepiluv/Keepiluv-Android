ec5d644a68f7a14c50af2b5ccea09f7dc960d2b6
chanho0908 <uni070@naver.com>
2026-03-06 19:49:27 +0900

    ♻️ Refactor: 직접 연결하기 텍스트 스타일 수정

--- feature/onboarding/src/main/java/com/twix/onboarding/couple/component/ConnectButton.kt
+++ feature/onboarding/src/main/java/com/twix/onboarding/couple/component/ConnectButton.kt
@@ -3,7 +3,6 @@ package com.twix.onboarding.couple.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,22 +11,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight.Companion.ExtraBold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.twix.designsystem.R
 import com.twix.designsystem.components.text.AppText
 import com.twix.designsystem.theme.CommonColor
 import com.twix.designsystem.theme.GrayColor
-import com.twix.designsystem.theme.NanumSquareNeoFamily
 import com.twix.designsystem.theme.TwixTheme
 import com.twix.domain.model.enums.AppTextStyle
 import com.twix.ui.extension.noRippleClickable
@@ -48,39 +43,26 @@ internal fun ConnectButton(
                     color = GrayColor.C500,
                     width = 1.2.dp,
                     shape = RoundedCornerShape(12.dp),
-                ).noRippleClickable { onClickConnect() },
+                ).noRippleClickable(onClick = onClickConnect),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Column(
-            modifier = Modifier.padding(start = 25.dp),
-            verticalArrangement = Arrangement.Center,
+            modifier =
+                Modifier
+                    .padding(start = 25.dp),
         ) {
             AppText(
                 text = stringResource(R.string.onboarding_couple_connect_direct_description),
                 style = AppTextStyle.C1,
                 color = GrayColor.C400,
+                modifier = Modifier.height(18.dp),
             )
-
-            Spacer(modifier = Modifier.height(3.dp))
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = stringResource(R.string.onboarding_couple_connect_direct),
-                    fontFamily = NanumSquareNeoFamily,
-                    fontWeight = ExtraBold,
-                    color = GrayColor.C500,
-                    fontSize = 16.sp,
-                    lineHeight = 22.2.sp,
-                )
-
-                AppText(
-                    text = stringResource(R.string.onboarding_couple_connect),
-                    style = AppTextStyle.T2,
-                    color = GrayColor.C500,
-                )
-            }
+            AppText(
+                text = stringResource(R.string.onboarding_couple_direct_connect_button_title),
+                style = AppTextStyle.T2,
+                color = GrayColor.C500,
+                modifier = Modifier.height(24.dp),
+            )
         }
 
         Spacer(modifier = Modifier.weight(1f))

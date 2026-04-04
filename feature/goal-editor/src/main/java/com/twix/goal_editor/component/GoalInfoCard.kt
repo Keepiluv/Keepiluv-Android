b23551fe98e2aa7f7677026cd1d77bf0098c9622
dogmania <hj296914@gmail.com>
2026-04-05 06:12:21 +0900

    ♻️ Refactor: 매일, 매주, 매월 컴포넌트 사이즈 고정

--- feature/goal-editor/src/main/java/com/twix/goal_editor/component/GoalInfoCard.kt
+++ feature/goal-editor/src/main/java/com/twix/goal_editor/component/GoalInfoCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -26,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.twix.designsystem.R
 import com.twix.designsystem.components.common.CommonSwitch
@@ -126,18 +128,24 @@ private fun RepeatTypeSettings(
             RepeatCycle.entries.forEachIndexed { index, type ->
                 val isSelected = selectedRepeatCycle == type
 
-                AppText(
-                    text = type.label(),
-                    style = AppTextStyle.B2,
-                    color = if (isSelected) CommonColor.White else GrayColor.C500,
+                Box(
                     modifier =
                         Modifier
+                            .size(width = 56.dp, height = 32.dp)
                             .clip(RoundedCornerShape(8.dp))
                             .background(if (isSelected) GrayColor.C500 else CommonColor.White)
                             .border(1.dp, GrayColor.C500, RoundedCornerShape(8.dp))
-                            .padding(horizontal = 12.dp, vertical = 5.5.dp)
+                            .padding(horizontal = 12.dp)
                             .noRippleClickable(onClick = { onSelectedRepeatType(type) }),
-                )
+                    contentAlignment = Alignment.Center,
+                ) {
+                    AppText(
+                        text = type.label(),
+                        style = AppTextStyle.B2,
+                        color = if (isSelected) CommonColor.White else GrayColor.C500,
+                        textAlign = TextAlign.Center,
+                    )
+                }
 
                 if (index != RepeatCycle.entries.lastIndex) Spacer(Modifier.width(8.dp))
             }

2b707f2bac67852dd6d4b1d1bb6ee989741bffe1
dogmania <hj296914@gmail.com>
2026-04-04 23:23:27 +0900

    ♻️ Refactor: 홈 이모지 아이콘 사이즈 변경

--- core/design-system/src/main/java/com/twix/designsystem/components/goal/GoalCardFrame.kt
+++ core/design-system/src/main/java/com/twix/designsystem/components/goal/GoalCardFrame.kt
@@ -56,7 +56,7 @@ fun GoalCardFrame(
             Image(
                 painter = painterResource(goalIcon.toRes()),
                 contentDescription = null,
-                modifier = Modifier.size(22.dp),
+                modifier = Modifier.size(32.dp),
             )
 
             Spacer(Modifier.width(10.dp))

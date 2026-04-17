05deb50352f055bb3bd991b7f67272ee63b989cd
chanho0908 <uni070@naver.com>
2026-04-17 12:40:17 +0900

    ♻️ Refactor: ProfileScreen.kt 내 import 문 정렬 수정

--- feature/onboarding/src/main/java/com/twix/onboarding/profile/ProfileScreen.kt
+++ feature/onboarding/src/main/java/com/twix/onboarding/profile/ProfileScreen.kt
@@ -23,10 +23,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle

9b8c9bd44a91e14c15732cf087ed658a51d1770d
dogmania <hj296914@gmail.com>
2026-04-30 20:02:24 +0900

    ♻️ Refactor: 가로세로 비율 추가

--- feature/goal-editor/src/main/java/com/twix/goal_editor/component/GoalInfoCard.kt
+++ feature/goal-editor/src/main/java/com/twix/goal_editor/component/GoalInfoCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -115,6 +116,7 @@ private fun RepeatTypeSettings(
     Column(
         modifier =
             Modifier
+                .aspectRatio(350 / 97f)
                 .padding(16.dp),
     ) {
         HeaderText(stringResource(R.string.word_repeat_type))

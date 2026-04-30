e8b14eac270581f7ae6edef05df4c7b7eea1583f
dogmania <hj296914@gmail.com>
2026-04-30 20:01:36 +0900

    ♻️ Refactor: 버튼 고정 높이 추가

--- core/design-system/src/main/java/com/twix/designsystem/components/button/AppButton.kt
+++ core/design-system/src/main/java/com/twix/designsystem/components/button/AppButton.kt
@@ -1,11 +1,15 @@
 package com.twix.designsystem.components.button
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -33,17 +37,20 @@ fun AppButton(
         border = border,
         onClick = onClick,
         enabled = enabled,
-        modifier = modifier,
+        modifier =
+            modifier
+                .height(52.dp),
     ) {
-        AppText(
-            text = text,
-            color = textColor,
-            style = AppTextStyle.T2,
-            textAlign = TextAlign.Center,
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 14.dp),
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            AppText(
+                text = text,
+                color = textColor,
+                style = AppTextStyle.T2,
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }

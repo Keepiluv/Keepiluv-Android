77acf51502abbb750ed56a29ca0a0fd775b7077d
dogmania <hj296914@gmail.com>
2026-04-30 20:27:43 +0900

    ♻️ Refactor: ktlint 적용

--- core/design-system/src/main/java/com/twix/designsystem/components/button/AppButton.kt
+++ core/design-system/src/main/java/com/twix/designsystem/components/button/AppButton.kt
@@ -3,9 +3,7 @@ package com.twix.designsystem.components.button
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable

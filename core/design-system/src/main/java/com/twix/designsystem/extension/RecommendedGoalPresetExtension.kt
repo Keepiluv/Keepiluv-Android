d3249042493e465766fc477a6df4fd2e580c7af7
dogmania <hj296914@gmail.com>
2026-05-07 20:21:37 +0900

    ✨ Feat: 추천 목표 타이틀 enum 및 확장 메서드 구현

---
+++ core/design-system/src/main/java/com/twix/designsystem/extension/RecommendedGoalPresetExtension.kt
@@ -0,0 +1,16 @@
+package com.twix.designsystem.extension
+
+import androidx.annotation.StringRes
+import com.twix.designsystem.R
+import com.twix.domain.model.enums.RecommendedGoalTitleKey
+
+@StringRes
+fun RecommendedGoalTitleKey.stringResId(): Int =
+    when (this) {
+        RecommendedGoalTitleKey.WORKOUT -> R.string.recommended_goal_workout
+        RecommendedGoalTitleKey.VITAMIN -> R.string.recommended_goal_vitamin
+        RecommendedGoalTitleKey.WALK -> R.string.recommended_goal_walk
+        RecommendedGoalTitleKey.READING -> R.string.recommended_goal_reading
+        RecommendedGoalTitleKey.CLEANING -> R.string.recommended_goal_cleaning
+        RecommendedGoalTitleKey.CALL_ON_WAY_HOME -> R.string.recommended_goal_call_on_way_home
+    }

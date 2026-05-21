c3a4660bce513da229313d0dc2efe16eaa6aa579
chanho0908 <uni070@naver.com>
2026-05-21 11:37:50 +0900

    🐛 Fix: 알림 초기 목록 요청 차단 수정

--- feature/notification/src/main/java/com/twix/notification/contract/NotificationUiState.kt
+++ feature/notification/src/main/java/com/twix/notification/contract/NotificationUiState.kt
@@ -10,7 +10,7 @@ data class NotificationUiState(
     val notificationList: List<Notification> = emptyList(),
     val hasNext: Boolean = true,
     override val hasLoadedContent: Boolean = false,
-    override val isLoading: Boolean = true,
+    override val isLoading: Boolean = false,
     override val error: AppError? = null,
 ) : ContentLoadableState {
     val canLoadNextPage: Boolean

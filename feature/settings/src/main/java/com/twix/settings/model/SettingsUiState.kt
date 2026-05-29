9489f9c56608e7916a4e38af343a67b9e248f4f4
dogmania <hj296914@gmail.com>
2026-05-29 18:11:53 +0900

    ♻️ Refactor: 상태 변수 초기값 변경

--- feature/settings/src/main/java/com/twix/settings/model/SettingsUiState.kt
+++ feature/settings/src/main/java/com/twix/settings/model/SettingsUiState.kt
@@ -6,7 +6,7 @@ import com.twix.ui.base.LoadableState
 data class SettingsUiState(
     val nickName: String = "",
     val email: String = "",
-    val pokeNotificationEnabled: Boolean = true,
+    val pokeNotificationEnabled: Boolean = false,
     val marketingNotificationEnabled: Boolean = false,
     val nightMarketingNotificationEnabled: Boolean = false,
     override val isLoading: Boolean = false,

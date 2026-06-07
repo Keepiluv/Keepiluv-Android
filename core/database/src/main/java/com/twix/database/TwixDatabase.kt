9b715fcc7d0a2f2a1798155816a855493fbd33cd
chanho0908 <uni070@naver.com>
2026-06-07 09:45:00 +0900

    🐛 Fix: 찌르기 히스토리 마이그레이션 추가

--- core/database/src/main/java/com/twix/database/TwixDatabase.kt
+++ core/database/src/main/java/com/twix/database/TwixDatabase.kt
@@ -2,13 +2,34 @@ package com.twix.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.twix.database.poke.PokeHistoryDao
 import com.twix.database.poke.PokeHistoryEntity
 
 @Database(
     entities = [PokeHistoryEntity::class],
-    version = 1,
+    version = 2,
 )
 abstract class TwixDatabase : RoomDatabase() {
     abstract fun pokeHistoryDao(): PokeHistoryDao
+
+    companion object {
+        val MIGRATION_1_2 =
+            object : Migration(1, 2) {
+                override fun migrate(db: SupportSQLiteDatabase) {
+                    db.execSQL("DROP TABLE IF EXISTS poke_history")
+                    db.execSQL(
+                        """
+                        CREATE TABLE IF NOT EXISTS poke_history (
+                            goalId INTEGER NOT NULL,
+                            targetDate TEXT NOT NULL,
+                            pokedAt INTEGER NOT NULL,
+                            PRIMARY KEY(goalId, targetDate)
+                        )
+                        """.trimIndent(),
+                    )
+                }
+            }
+    }
 }
